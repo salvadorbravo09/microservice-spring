a5ead52fc1fdbc5caf2864d3791f24ea6c85e216
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-10-13 18:41:28 -0300

    se creo un enumerador para la categoria de un website

---
+++ src/main/java/com/debuggeandoideas/companies_crud/entities/Category.java
@@ -0,0 +1,11 @@
+package com.debuggeandoideas.companies_crud.entities;
+
+public enum Category {
+    SOCIAL_NETWORK,
+    SERVICES,
+    STREAMING,
+    CLOUD_COMPUTING,
+    DEVICES,
+    EDUCATION,
+    NONE
+}
