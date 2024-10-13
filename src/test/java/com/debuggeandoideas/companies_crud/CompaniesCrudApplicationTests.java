39260a361de1bb0f98c5364f69d994a4a1306022
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-10-12 22:11:30 -0300

    primer commit

---
+++ src/test/java/com/debuggeandoideas/companies_crud/CompaniesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debuggeandoideas.companies_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
